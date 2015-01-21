db9a909b492a4b234916ad7d22dbc24ff7158899
Joserex65 <unconfigured@null.spigotmc.org>
2015-01-21 18:41:11 +0100

    Añadidos los métodos onEnable y onDisable
    Actualización simple

---
+++ src/me/Joserex65/Tutorial/Tutorial.java
@@ -0,0 +1,20 @@
+package me.Joserex65.Tutorial;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Tutorial extends JavaPlugin{
+	
+	@Override
+	public void onEnable(){
+		this.getServer().getConsoleSender().sendMessage(ChatColor.BLUE + "Plugin Tutorial activado correctamente. Versión actual: 1.0");
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		this.getServer().getConsoleSender().sendMessage(ChatColor.BLUE + "Plugin Tutorial desactivado correctamente. Versión actual: 1.0");
+	}
+	
+
+}
